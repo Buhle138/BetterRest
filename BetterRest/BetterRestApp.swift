2ed6fe245fd0f9cd999a59007cf7f4e9513ca613
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-02 22:49:21 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Buhle Radzilani on 2024/06/02.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
