a058daf7651b1b930953735277f3c47f8e74f226
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-03 00:15:46 +0200

    This is the basic layout with steppers for the desired amount of sleep and also the daily coffee.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeup = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack{
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeup, displayedComponents: .hourAndMinute).labelsHidden()
+                
+                //adding a stepper for users to choose how much sleep they want.
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                //Determining how much coffee they drink.
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(coffeeAmount == 1 ? "1 cup " : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                
+            }
+            
+            .navigationTitle("BetterRest")
+            .toolbar{
+                Button("Calculate", action: calculatedBedTime)
+            }
+            
         }
-        .padding()
+    }
+    func calculatedBedTime() {
+        
     }
 }
 
