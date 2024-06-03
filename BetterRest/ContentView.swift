2cd117fe998bb6b640962b8274a002626dc2317b
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-03 16:03:20 +0200

    Connecting Swiftui to Core ML. in order for it to be able to predict your bed time.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,12 +5,16 @@
 //  Created by Buhle Radzilani on 2024/06/02.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeup = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView{
@@ -39,11 +43,37 @@ struct ContentView: View {
             .toolbar{
                 Button("Calculate", action: calculatedBedTime)
             }
+            .alert(alertTitle, isPresented: $showingAlert){
+                Button("OK"){}
+            } message: {
+                Text(alertMessage)
+            }
             
         }
     }
     func calculatedBedTime() {
+        do{
+            let config = MLModelConfiguration()
+            
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeup)
+            
+            let hour =  (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeup - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is.."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        }catch{
+            alertTitle = "Error"
+            alertMessage = " Sorry, there was a problem calculating your bed time."
+        }
         
+        showingAlert = true
     }
 }
 
